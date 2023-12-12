b8d08c29a35fc45eff6d7c48ca886b1ede72e4ab
karunakeshari <karunakeshari884@gmail.com>
2023-12-12 17:23:28 +0530

    Count the Digits That Divide a Number

---
+++ Count the Digits That Divide a Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int temp=num;
+        int ans=0;
+        while(temp>0){
+            int dig=temp%10;
+            if(num%dig==0) ans++;
+            temp/=10;
+        }
+        return ans;
+    }
+};
+//Time Complexity-O(logn) base 10
+//Space Complexity-O(1)
